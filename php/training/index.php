b4f3059659f18af19604b6501f260ab43c074f9e
Ahmed Ewees Korany <ahmedewaisvi34@gmail.com>
2024-08-15 21:53:52 +0300

    add params to querfunc to get single post

--- php/training/index.php
+++ php/training/index.php
@@ -6,5 +6,8 @@ require "Database.php";
 $config = require "config.php";
 
 $db  = new Database($config['database']);
-$posts = $db->query("select * from posts")->fetchAll();
+
+$id=$_GET['id'];
+$query = "select * from posts where id =?";
+$posts = $db->query($query,[$id])->fetchAll();
 dd($posts);
