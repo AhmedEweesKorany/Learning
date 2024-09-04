e6ea52a7141a4a022556f0d0d12e14ddde1dc3ef
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2024-09-04 11:58:37 +0300

    create interface and use it to impelement diffrent classes with diffrent logic

--- php/oop/index.php
+++ php/oop/index.php
@@ -16,3 +16,30 @@ var_dump($employee);
 // echo $user->name;
 
 // var_dump($user->dd());
+
+
+## ====Abstraction ==== //
+
+interface Shape{
+    // can not include properties
+    // every method should be public 
+    // methods can not have body
+    // every one use this interface should implement his code structure and should include all interface method
+    public function calcArea(int $width,int $height);
+}
+
+class Triangle implements Shape{    
+    public function calcArea($width,$height)
+    {
+        return $width * $height * 0.5;
+
+    }
+}
+
+
+class Rectangle implements Shape{
+    public function calcArea(int $width, int $height)
+    {
+        return $width * $height;
+    }
+}
