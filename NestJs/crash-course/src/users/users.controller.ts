716d29e935227752ad4b0ddaa19f12d5ccac343e
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2025-06-11 13:44:24 +0300

    feat: Implement user management with CRUD operations and role-based filtering

--- NestJs/crash-course/src/users/users.controller.ts
+++ NestJs/crash-course/src/users/users.controller.ts
@@ -1,23 +1,69 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
-
-@Controller('users')// which mean /users
-
-
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { UsersService } from './users.service';
+const arr:any = [{
+  id: '1',
+  name: 'John Doe',
+  email: 'john@example.com',
+}]
+@Controller('users') // which mean /users
 export class UsersController {
-    @Get() // which mean /users
-    getUsers(){ // get users
-        return "get users 2ewds";
-    }
-    
-    @Get(':id')  // :id is a parameter
-    getUserById(@Param('id')id:string){ // GET SINGLE USER
-        return `get user by id ${id}`;
-    }
-    
-    @Post()
-    createUser(@Body() user:{}){
-        return user;
-    }
+  constructor(private readonly UsersService: UsersService) {}
 
+  @Get()
+  getUsers(@Query('role') role?:"INTERN" | "ADMIN") {
+   if(role === "INTERN" || role === "ADMIN"){
+     return this.UsersService.users.filter((user) => user.role === role);
+   }
+   return {
+    message:"you are not allowed to see this",
+   };
+  }
+  @Get('/interns')
+  getUser2() {
+    return {
+      id: '1',
+      name: 'John Doe',
+      email: 'john@example.com',
+    };
+  }
+
+  @Get(':id')
+  getUserById(@Param('id') id: string) {
+    return {
+      id,
+      name: 'John Doe',
+      email: 'john@example.com',
+    };
+  }
+
+  @Post()
+  createUser(@Body() user: Record<string, any>) {
+    return {
+      id: user.id,
+      name: user.name,
+    };
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() userUpdate: {}) {
+    return {
+      id,
+      ...userUpdate,
+    };
+  }
+
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return arr.filter((user) => user.id !== id);
+  }
 }
-
--- NestJs/crash-course/src/users/users.service.ts
+++ NestJs/crash-course/src/users/users.service.ts
@@ -1,4 +1,57 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  public users = [
+        {
+            "id": 1,
+            "name": "Leanne Graham",
+            "email": "Sincere@april.biz",
+            "role": "INTERN",
+        },
+        {
+            "id": 2,
+            "name": "Ervin Howell",
+            "email": "Shanna@melissa.tv",
+            "role": "INTERN",
+        },
+        {
+            "id": 3,
+            "name": "Clementine Bauch",
+            "email": "Nathan@yesenia.net",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 4,
+            "name": "Patricia Lebsack",
+            "email": "Julianne.OConner@kory.org",
+            "role": "ENGINEER",
+        },
+        {
+            "id": 5,
+            "name": "Chelsey Dietrich",
+            "email": "Lucio_Hettinger@annie.ca",
+            "role": "ADMIN",
+        }
+    ]
+
+
+    findAll(role?: "INTERN" | "ADMIN" | "ENGINEER") {
+        if (role) {
+            return this.users.filter(user => user.role === role);
+        }
+        return this.users;
+    }
+
+    findOne(id: number) {
+        const user= this.users.find(user => user.id === id);
+        return user ? user : { message: 'This User is not found' };
+    }
+
+    create(user: { id: number; name: string; email: string; role: string }) {
+        this.users.push(user);
+        return user;
+    }
+
+
+}
