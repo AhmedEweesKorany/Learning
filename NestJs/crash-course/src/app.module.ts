00882ef1ed201246309c51c4f918e35b12fa58c0
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2025-06-26 19:02:40 +0300

    feat: Add Property module and mapped types for DTOs
    This commit introduces the Property module and necessary dependencies for defining Data Transfer Objects (DTOs):
    
    - Added `@nestjs/mapped-types` as a dependency to facilitate the creation of DTOs based on entity types.
    - Imported and included the `PropertyModule` in the `AppModule` to integrate property-related functionalities into the application.

--- NestJs/crash-course/src/app.module.ts
+++ NestJs/crash-course/src/app.module.ts
@@ -5,11 +5,12 @@ import { UsersModule } from './users/users.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { pgConfig } from 'dbConfig';
 import { ConfigModule } from '@nestjs/config';
+import { PropertyModule } from './property/property.module';
 
 @Module({
   imports: [UsersModule,TypeOrmModule.forRoot(pgConfig),ConfigModule.forRoot({
       isGlobal: true, // makes it available across the app
-    }),],
+    }), PropertyModule,],
   controllers: [AppController],
   providers: [AppService],
 })
