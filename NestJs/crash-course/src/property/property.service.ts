d292d9981b6b93fc2bc8186de272484267a604b7
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2025-06-27 22:59:48 +0300

    feat(property): Implement full CRUD operations
    - Added database interactions for all operations.
    - Implemented error handling with HTTP exceptions.
    - Updated return types to include data and messages.
    - Improved find and findOne methods.
    - Added remove method.

--- NestJs/crash-course/src/property/property.service.ts
+++ NestJs/crash-course/src/property/property.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { CreatePropertyDto } from './dto/create-property.dto';
 import { UpdatePropertyDto } from './dto/update-property.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -29,19 +29,39 @@ export class PropertyService {
    }
   }
 
-  findAll() {
-    return `This action returns all property`;
+  async findAll() {
+    return await this.propertyRepository.find();
+  }
+    async findOne(id: number) {
+      const property = await this.propertyRepository.findOne({where:{id}});
+
+      if (!property) {
+        throw new HttpException('Property not found', HttpStatus.NOT_FOUND);
+      }
+
+      return property;
+    }
+
+  async update(id: number, updatePropertyDto: UpdatePropertyDto) {
+    const property = await this.propertyRepository.findOne({where:{id}});
+    if (!property) {
+      throw new HttpException('Property not found', HttpStatus.NOT_FOUND);
+    }
+    await this.propertyRepository.update(id, updatePropertyDto);
+    return {
+      message: 'Property updated successfully',
+      data: updatePropertyDto,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} property`;
-  }
-
-  update(id: number, updatePropertyDto: UpdatePropertyDto) {
-    return `This action updates a #${id} property`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} property`;
+ async remove(id: number) {
+    const property = await this.propertyRepository.findOne({where:{id}});
+    if (!property) {
+      throw new HttpException('Property not found', HttpStatus.NOT_FOUND);
+    }
+   await  this.propertyRepository.delete(id);
+    return {
+      message: 'Property deleted successfully',
+    };
   }
 }
