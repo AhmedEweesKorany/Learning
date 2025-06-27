d302c2938c44ad4edaafbc7b6a795094d8eaebaa
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2025-06-27 18:44:42 +0300

    - 2025-06-27 18:44:41

--- NestJs/crash-course/src/property/property.module.ts
+++ NestJs/crash-course/src/property/property.module.ts
@@ -8,6 +8,6 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 @Module({
   imports: [TypeOrmModule.forFeature([Property])],
   controllers: [PropertyController],
-  providers: [PropertyService],
+  providers: [PropertyService], // Add the PropertyService here
 })
 export class PropertyModule {}
