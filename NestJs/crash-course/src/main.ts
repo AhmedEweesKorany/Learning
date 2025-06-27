edab75c2712c80cf1b84d8579fd41928b3ea9f87
AhmedEweesKorany <ahmedewaisvi34@gmail.com>
2025-06-27 17:52:32 +0300

    updated changes in  - 2025-06-27 17:52:32

--- NestJs/crash-course/src/main.ts
+++ NestJs/crash-course/src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // strips properties that are not in the DTO
+    forbidNonWhitelisted: true, // throws an error if non-whitelisted properties are found
+    transform: true, // automatically transforms payloads to DTO instances
+  }));
   await app.listen(process.env.PORT ?? 3000,()=>{
     console.log("nest app is running on port at http://localhost:3000");
   });
